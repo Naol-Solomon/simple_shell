a80413ab301296e85c31d4311fda2f80daf4fb40
NathanielBirhanu <nbirhanu98@gmail.com>
2023-09-19 13:36:28 -0700

    update

--- get_location.c
+++ get_location.c
@@ -1,20 +1,28 @@
 #include "main.h"
+/**
+ * get_location - Retrieves the location of a command in the system's PATH
+ * @command: The command to search for
+ *
+ * Return: The location of the command if found, NULL otherwise
+ */
 
 char *get_location(char *command)
 {
-    char *path, *pathCopy, *pathToken, *filePath;
-    int commandLength, directoryLength;
-    struct stat buff;
+	char *path, *pathCopy, *pathToken, *filePath;
+	int commandLength, directoryLength;
+	struct stat buff;
 
-    path = getenv("PATH");
-    if(path){
-        pathCopy = strdup(path);
-        commandLength = strlen(command);
-        pathToken = strtok(pathCopy, ":");
+	path = getenv("PATH");
+	if (path)
+	{
+		pathCopy = strdup(path);
+		commandLength = strlen(command);
+		pathToken = strtok(pathCopy, ":");
 
-        while (pathToken != NULL){
-            directoryLength = strlen(pathToken);
-            filePath = malloc(commandLength + directoryLength + 2 + 1);
+		while (pathToken != NULL)
+		{
+			directoryLength = strlen(pathToken);
+			filePath = malloc(commandLength + directoryLength + 2 + 1);
 
             strcpy (filePath, pathToken);
             strcat (filePath, "/");
